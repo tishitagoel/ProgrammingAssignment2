4c010816d230271762295a272ed485861a7d4536
Tishita Goel <81733655+tishitagoel@users.noreply.github.com>
2021-05-02 12:05:42 +0530

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Writing a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL #Setting the inverse to a null value, yet to be computed
+  s <- function(z) {
+    x <<- z ##Storing the x matrix outside of the current environment
+    inv <<- NULL #everytime there's a change, inv becomes null
+  }
+  g <- function() {x} ##Returns the x matrix
+  ssolve <- function(solve) {inv <<- solve} ##Storing the inverse outside of the current environment
+  gsolve <- function() {inv} ##Returns the inv that has been computed
+  list(s = s, g = g,
+       ssolve = ssolve,
+       gsolve = gsolve)
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated,
+# then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$gsolve() #get the inverse from list
+  if(!is.null(inv)) {
+    message("retrieving inverse")
+    return(inv) #inverse returned if not null
+  }
+  data <- x$g() #Otherwise the called matrix is retrieved from the list
+  i <- solve(data, ...) #calculate the inverse
+  x$ssolve(inv) #cache the inverse in the list
+  inv #inverse matrix of x returned to the user
 }
+
+
+#Testing the code
+B <- matrix(c(1,2,3,4),2,2)
+solve(B)
